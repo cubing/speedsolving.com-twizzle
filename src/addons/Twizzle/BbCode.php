146f56ac4c7d36fa74886b24897404514b1bc400
Lucas Garron <code@garron.net>
2022-11-26 02:36:50 -0800

    Update add-on.

--- src/addons/Twizzle/BbCode.php
+++ src/addons/Twizzle/BbCode.php
@@ -4,29 +4,67 @@ namespace Twizzle;
 
 class BbCode
 {
-	public static function matchCallback($url, $matchedId, \XF\Entity\BbCodeMediaSite $site, $siteId)
-	{
-		return urlencode($url);
-	}
+    public static function bbcodeCallback($tagChildren, $tagOption, $tag, array $options, \XF\BbCode\Renderer\AbstractRenderer $renderer)
+    {
+        $rendered = $renderer->renderSubTreePlain($tagChildren, $options);
+        if (preg_match("/\[URL[^\]]*\](https:\/\/(alpha|beta)\.twizzle\.net\/.*)\[\/URL\]/i", $rendered, $match) === 1) {
+          $url = $match[1];
+        } else if (preg_match('/\[MEDIA=twizzle_link_encoded\](https%3A%2F%2F(alpha|beta)\.twizzle\.net%2F.*)\[\/MEDIA\]/i', $rendered, $match) === 1) {
+          $url = urldecode($match[1]);
+        } else {
+            $puzzle = $tag["option"];
+            $contents = $tag["children"][0];
+            $url = "https://alpha.twizzle.net/edit/?";
+            if ($puzzle) {
+                $url .= "puzzle=" . urlencode($puzzle) . "&";
+            }
+            if (is_string($contents) && $contents) {
+                $url .= "alg=" . urlencode($contents);
+            }
+        }
 
-	public static function htmlCallback($mediaKey, array $site, $siteId)
-	{
-		$url = urldecode($mediaKey);
-		$html_alg = "";
+        $html_alg = "";
+        $url_components = parse_url($url);
+        $href_url = "https://" . $url_components["host"] . $url_components["path"];
+        if (array_key_exists("query", $url_components)) {
+          parse_str($url_components["query"], $url_params);
+          if (array_key_exists("alg", $url_params)) {
+            $html_alg = $url_params["alg"];
+          }
+          $href_url .= "?" . http_build_query($url_params);
+        }
 
-		$url_components = parse_url($url);
-		if (array_key_exists('query', $url_components)) {
-			parse_str($url_components['query'], $url_params);
-			if (array_key_exists('alg', $url_params)) {
-				$html_alg = $url_params['alg'];
-			}
-		}
+        return '<div class="bbMediaWrapper">
+<twizzle-forum-link><a href="' . $href_url . '">Twizzle link</a><pre style="margin: 0">' . $html_alg . '</pre></twizzle-forum-link>
+<script>
+if (!globalThis.twizzleLinkScript) {
+  var script = document.createElement("script");
+  globalThis.twizzleLinkScript = script;
+  script.src = "/misc/twizzle/js/index.js";
+  script.type = "module";
 
-		return \XF::app()->templater()->renderTemplate('public:_media_site_embed_twizzle_link_encoded', [
-			'id' => $url,
-			'link_url' => $url,
-			'html_alg' => $html_alg
-		]);
-	}
+  function append() {
+    document.body.appendChild(script);
+    const style = document.createElement("style");
+    style.textContent = `
+@font-face {
+  font-family: "Ubuntu";
+  src: url("/misc/twizzle/font/ubuntu/Ubuntu-Regular.ttf");
 }
+twizzle-forum-link {
+  font-family: Ubuntu, -apple-system, Tahoma, sans-serif;
+}
+`;
+    document.body.appendChild(style);
+  }
 
+  if (document.body) {
+    append();
+  } else {
+    window.addEventListener("DOMContentLoaded", append);
+  }
+}
+</script>
+</div>';
+    }
+}
