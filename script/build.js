c6749ef71a0911deb45242e2794c5109a44853b4
Lucas Garron <code@garron.net>
2022-11-26 03:10:58 -0800

    Build add-on with hashes.

--- script/build.js
+++ script/build.js
@@ -1,9 +1,14 @@
 import { barelyServe } from "barely-a-dev-server";
 import { build } from "esbuild";
+import { createHash } from "node:crypto";
 
-import { cp } from "node:fs/promises";
+import { cp, readdir, readFile, writeFile } from "node:fs/promises";
+import { basename, join } from "node:path";
 
-await cp("./src/static", "dist/", { recursive: true });
+const ADDON_SERVER_PATH = "src/addons/Twizzle";
+const ADDON_DIST_DIR = `./dist/www.speedsolving.com/${ADDON_SERVER_PATH}`;
+
+await cp("./src/static", "./dist/", { recursive: true });
 
 build({
   format: "esm",
@@ -15,3 +20,27 @@ build({
   minify: true,
   sourcemap: true,
 });
+
+await cp("./src/addons/Twizzle", ADDON_DIST_DIR, { recursive: true });
+
+const dir = await readdir(ADDON_DIST_DIR);
+const hashes = {};
+for (const fileName of dir) {
+  if ([".DS_Store"].includes(fileName)) {
+    continue;
+  }
+  const sha256HashInstance = createHash("sha256");
+
+  // // wat
+  // let contents = await readFile(join(ADDON_DIST_DIR, fileName), "ascii");
+  // contents = contents.replaceAll("\r", "");
+  // sha256HashInstance.update(contents, "ascii");
+
+  let contents = await readFile(join(ADDON_DIST_DIR, fileName));
+  sha256HashInstance.update(contents);
+  hashes[`${ADDON_SERVER_PATH}/${fileName}`] = sha256HashInstance.digest("hex");
+}
+await writeFile(
+  join(ADDON_DIST_DIR, "hashes.json"),
+  JSON.stringify(hashes, null, "  "),
+);
