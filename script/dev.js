53663a83fe85fc4e931f6d22f87f7c7d9304bed0
Lucas Garron <code@garron.net>
2022-12-08 19:07:01 -0800

    Add `make dev`.

---
+++ script/dev.js
@@ -0,0 +1,22 @@
+import { build } from "esbuild";
+import { createHash } from "node:crypto";
+import { readdir, stat } from "node:fs/promises";
+import { join } from "node:path";
+
+import { cp, readFile, writeFile } from "node:fs/promises";
+import { barelyServe } from "barely-a-dev-server";
+
+// build({
+//   format: "esm",
+//   target: "es2020",
+//   bundle: true,
+//   splitting: true,
+//   entryPoints: ["./src/js/index.ts"],
+//   outdir: "./dist/www.speedsolving.com/misc/twizzle/js/",
+//   minify: true,
+//   sourcemap: true,
+// });
+
+barelyServe({
+  entryRoot: "./src/js",
+})
