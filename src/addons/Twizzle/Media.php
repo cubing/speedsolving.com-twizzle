1c02847dcf95faa441495fedf1e6cde6191ce06e
Lucas Garron <code@garron.net>
2023-09-06 22:44:23 -0700

    [Media.php] Change the `alg` template var name.

--- src/addons/Twizzle/Media.php
+++ src/addons/Twizzle/Media.php
@@ -9,23 +9,24 @@ class Media
 		return urlencode($url);
 	}
 
+	// TODO: figure out how to use/match the BbCode fallback.
 	public static function htmlCallback($mediaKey, array $site, $siteId)
 	{
 		$url = urldecode($mediaKey);
-		$html_alg = "";
+		$alg = "";
 
 		$url_components = parse_url($url);
 		if (array_key_exists('query', $url_components)) {
 			parse_str($url_components['query'], $url_params);
 			if (array_key_exists('alg', $url_params)) {
-				$html_alg = $url_params['alg'];
+				$alg = $url_params['alg'];
 			}
 		}
 
 		return \XF::app()->templater()->renderTemplate('public:_media_site_embed_twizzle_link_encoded', [
 			'id' => $url,
 			'link_url' => $url,
-			'html_alg' => $html_alg
+			'alg' => $alg
 		]);
 	}
 }
